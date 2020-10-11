1781dcb23c5bba86e5ba5e81bbc744e60a058cc0
yukinoshiita <850465769@qq.com>
2020-10-11 00:41:07 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+  Project SIF Card Auto Assembler
+  main.cpp
+Purpose: Take a database of cards and assemble a proper team according to specified requirement.
+
+Author sxt
+Version 1.00  - 11/10/2020
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+}
